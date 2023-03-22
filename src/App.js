08b62014ab28ce87a36bd145df6accb6b28c6432
Cirovsky <cirovisqui@gmail.com>
2023-03-22 19:31:48 -0300

    criada classe equipamento para gerir dos dados

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ import eqStateHistory from './data/equipmentStateHistory.json';
 
 function App() {
   console.log("equipment", equipment);
-  console.log("eqModel", eqModel);
+  console.log("eqModel", eqModel);//id == equipament model id de equipment
   console.log("eqPositionHistory", eqPositionHistory);
   console.log("eqState", eqState);
-  console.log("eqStateHistory ",eqStateHistory)
+  console.log("eqStateHistory ",eqStateHistory);
+
+  class Equipment{
+    constructor(name, id, modelId, modelName, historyPosition, eqState, stateHistory){
+      this.name = name;
+      this.id = id;
+      this.modelId = modelId;
+      this.modelName = modelName;
+      this.historyPosition = historyPosition;
+      this.eqState = eqState;
+      this.stateHistory = stateHistory;
+    }
+  }
 
   return (
     <div className="App">

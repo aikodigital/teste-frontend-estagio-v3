15f2834b94519a76b37db53746829c4ef7319d1f
Cirovsky <cirovisqui@gmail.com>
2023-03-22 20:23:55 -0300

    class Equipment e array de objetos correspondentes

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import eqModel from './data/equipmentModel.json';
 import eqPositionHistory from './data/equipmentPositionHistory.json';
 import eqState from './data/equipmentState.json';
 import eqStateHistory from './data/equipmentStateHistory.json';
+import { equipments } from './entities/equipment';
 
-function App() {
-  console.log("equipment", equipment);
+
+/* console.log("equipment", equipment);
   console.log("eqModel", eqModel);//id == equipament model id de equipment
   console.log("eqPositionHistory", eqPositionHistory);
   console.log("eqState", eqState);
-  console.log("eqStateHistory ",eqStateHistory);
+  console.log("eqStateHistory ",eqStateHistory); */
 
-  class Equipment{
-    constructor(name, id, modelId, modelName, historyPosition, eqState, stateHistory){
-      this.name = name;
-      this.id = id;
-      this.modelId = modelId;
-      this.modelName = modelName;
-      this.historyPosition = historyPosition;
-      this.eqState = eqState;
-      this.stateHistory = stateHistory;
-    }
-  }
+function App() {
+
+  
+
+
 
   return (
     <div className="App">
---
+++ src/entities/equipment.js
@@ -0,0 +1,59 @@
+import equipment from '../data/equipment.json';
+import eqModel from '../data/equipmentModel.json';
+import eqPositionHistory from '../data/equipmentPositionHistory.json';
+import eqState from '../data/equipmentState.json';
+import eqStateHistory from '../data/equipmentStateHistory.json';
+
+console.log("equipment", equipment);
+  console.log("eqModel", eqModel);//id == equipament model id de equipment
+  console.log("eqPositionHistory", eqPositionHistory);
+  console.log("eqState", eqState);
+  console.log("eqStateHistory ",eqStateHistory);
+
+  class Equipment{
+    constructor(name, id, modelId, modelName, positionHistory, stateHistory){
+      this.name = name;
+      this.id = id;
+      this.modelId = modelId;
+      this.modelName = modelName;
+      this.positionHistory = positionHistory;
+      this.stateHistory = stateHistory;
+    }
+  }
+
+  const equipments = [];
+
+  equipment.forEach( eq =>{
+    equipments.push(
+        new Equipment(
+            eq.name, 
+            eq.id, 
+            eq.equipmentModelId, 
+            findModelName(eq.equipmentModelId),
+            findPositionHistory(eq.id),
+            findStateHistory(eq.id)
+            ))
+  })
+
+  function findModelName(modelId){
+    const modelName =  eqModel.filter(model => model.id == modelId).map(model => model.name)[0];
+    return modelName;
+  }
+
+  function findPositionHistory(id){
+    const positions = eqPositionHistory.filter(eq => eq.equipmentId === id).map(eq => eq.positions)[0];
+    return positions;
+  }
+
+  function findStateHistory(id){
+    const stateHistory = eqStateHistory.filter(eq => eq.equipmentId === id).map(eq => eq.states)[0];
+    console.log("state Position:",stateHistory);
+    return stateHistory;
+  }
+
+  export {
+    equipments
+  }
+
+
+

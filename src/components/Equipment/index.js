e0f8c4389394c8fc503f158501bb85dc7a3ff223
Cirovsky <cirovisqui@gmail.com>
2023-03-22 20:41:12 -0300

    comp. ShowEquipments e Equipment com depedência

---
+++ src/components/Equipment/index.js
@@ -0,0 +1,13 @@
+const Equipment = ({ equipment }) => {
+    return (
+        <li>
+            <h2>{equipment.name}</h2>
+            <h3>{equipment.modelName}</h3>
+            <h3></h3>
+            
+            
+        </li>
+    )
+}
+
+export default Equipment;

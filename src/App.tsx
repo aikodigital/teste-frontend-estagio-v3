7b045bfdf25329586a959f97c3f370e25ed94593
Pedro Mendes <pedromendescraft@gmail.com>
2023-03-23 12:14:29 -0300

    feat: Simple header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from './components/Header';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-slate-600 p-4 px-20 text-white">
+      <h1 className="text-2xl font-bold">Status de equipamentos</h1>
+    </header>
+  );
+}
+
+export default Header;

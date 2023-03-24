cbd5a6832802ec988b886307d2b106bb38c581c6
Cirovsky <cirovisqui@gmail.com>
2023-03-24 13:14:06 -0300

    manipulando icone do Marker

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,16 +1,36 @@
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import './Map.css';
 import Report from '../Report';
+import { eqState } from '../../entities/equipment';
+import { DivIcon, Icon, divIcon } from 'leaflet';
 
 const position = [-19, -46];
 
+const icon = new Icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/25/25613.png',
+  iconSize:[50,50]
+})
+
+const newIcon = new DivIcon({
+  className: 'equipment-icon',
+})
 
 const Map = ({equipments}) => {
 
     const markers = equipments.map( equipment => {
+        const color = eqState.filter( state =>{
+          if(state.id === equipment.lastState().equipmentStateId){
+            return state;
+          }
+        })[0].color;
+
         const coordinates = equipment.lastPosition();
         return (
-        <Marker key={equipment.id} position={[coordinates.lat, coordinates.lon]}>
+        <Marker
+          icon={newIcon}
+          key={equipment.id} 
+          position={[coordinates.lat, coordinates.lon]}
+          >
             <Popup key={equipment.id}>
                 <Report key={equipment.id} equipment ={equipment}/>
             </Popup>

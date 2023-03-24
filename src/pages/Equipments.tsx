f43edd2bd0dc4cf9c9d73712a8d670cb351284eb
Pedro Mendes <pedromendescraft@gmail.com>
2023-03-24 18:34:50 -0300

    feat: Simple state info on equipments page

--- src/pages/Equipments.tsx
+++ src/pages/Equipments.tsx
@@ -1,6 +1,8 @@
 import { query } from '../utils/query';
 import equipment from '../assets/data/equipment.json';
 import equipmentModel from '../assets/data/equipmentModel.json';
+import equipmentStateHistory from '../assets/data/equipmentStateHistory.json';
+import equipmentState from '../assets/data/equipmentState.json';
 import Card from '../components/Card';
 
 function Equipments() {
@@ -13,13 +15,30 @@ function Equipments() {
   });
   return (
     <div className="flex flex-wrap justify-center">
-      {equipments.map((equipment) => (
-        <Card key={equipment.id}>
-          <p className="opacity-70">{equipment.id}</p>
-          <h2 className="text-xl">{equipment.name}</h2>
-          <p className="opacity-70">{equipment.relation.first()?.name}</p>
-        </Card>
-      ))}
+      {equipments.map((equipment) => {
+        const firstState = query(
+          query(equipmentStateHistory)
+            .where(({ equipmentId }) => equipment.id === equipmentId)
+            .first().states,
+        )
+          .sort('date', 'desc')
+          .relation(equipmentState, 'id', 'equipmentStateId')[0]
+          .relation.first();
+        return (
+          <Card key={equipment.id}>
+            <p className="opacity-70">{equipment.id}</p>
+            <h2 className="text-xl">{equipment.name}</h2>
+            <p className="opacity-70">{equipment.relation.first()?.name}</p>
+            <p className="flex items-center text-xs opacity-70">
+              {firstState.name}{' '}
+              <div
+                className="m-1 h-3 w-3 rounded-full"
+                style={{ backgroundColor: firstState.color }}
+              />
+            </p>
+          </Card>
+        );
+      })}
     </div>
   );
 }

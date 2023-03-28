e6214b7442abdbc9be2372a373ff15a7c2d96ef7
rodrfentanes21 <rfentanes@sga.pucminas.br>
2023-03-28 09:53:31 -0300

    refactored formatDate

--- react-ts/src/components/Popup.tsx
+++ react-ts/src/components/Popup.tsx
@@ -28,14 +28,14 @@ function traduzEstado(estadoId: string) {
   return "Estado não encontrado";
 }
 
-function formatDate(dateString: string) {
+function formatDate(dateString: string): string {
   const date = new Date(dateString);
-  const day = date.getDate().toString().padStart(2, "0");
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear().toString();
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  return `${day}/${month}/${year} ${hours}:${minutes}`;
+  return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}/${date.getFullYear().toString()} ${date
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
 }
 
 import MapDisplay from "./MapDisplay";

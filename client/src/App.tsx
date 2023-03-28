e7544ee61f9008a47ac1117ffd967a3d9b1d652c
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-03-28 01:11:48 -0300

    feature/Scroll logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,22 @@
+//@ts-nocheck
 import "../src/css/global.css";
-
+import { useState, useEffect, createContext } from "react";
 import { Map } from "./components/Map/Map";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import { createEquipmentArray } from "./utils/equipmentList";
 
+export const EquipmentContext = createContext();
 const equipmentArray = createEquipmentArray();
 
-equipmentArray.forEach((equipment) => {
-  console.log(equipment.getMostRecentState());
-});
-
 function App() {
+  const [equipmentIndex, setEquipmentIndex] = useState(0);
+
   return (
     <section className="container">
-      <Sidebar equipments={equipmentArray}/>
-      <Map lat={-19.192595} lon={-46.061072} equipments={equipmentArray} />
+      <EquipmentContext.Provider value={[equipmentIndex, setEquipmentIndex]}>
+        <Sidebar equipments={equipmentArray} />
+        <Map lat={-19.192595} lon={-46.061072} equipments={equipmentArray} />
+      </EquipmentContext.Provider>
     </section>
   );
 }
--- client/src/components/Scroll/Scroll.tsx
+++ client/src/components/Scroll/Scroll.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import * as ScrollArea from "@radix-ui/react-scroll-area";
 import { Card } from "../Card/Card";
 
@@ -7,25 +7,44 @@ interface Props {
   isChild: boolean;
 }
 
-export const ScrollAreaComp: React.FC<Props> = ({ components, isChild }) => (
-  <ScrollArea.Root className="ScrollAreaRoot">
-    <ScrollArea.Viewport className={ isChild ? "ScrollAreaChild" : "ScrollAreaViewport"}>
-      {components.map((component, index) => (
-        <React.Fragment key={index}>{component}</React.Fragment>
-      ))}
-    </ScrollArea.Viewport>
-    <ScrollArea.Scrollbar
-      className="ScrollAreaScrollbar"
-      orientation="vertical"
-    >
-      <ScrollArea.Thumb className="ScrollAreaThumb" />
-    </ScrollArea.Scrollbar>
-    <ScrollArea.Scrollbar
-      className="ScrollAreaScrollbar"
-      orientation="horizontal"
-    >
-      <ScrollArea.Thumb className="ScrollAreaThumb" />
-    </ScrollArea.Scrollbar>
-    <ScrollArea.Corner className="ScrollAreaCorner" />
-  </ScrollArea.Root>
-);
+const scrollTo = 6;
+
+export const ScrollAreaComp: React.FC<Props> = ({ components, isChild }) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      const element = scrollRef.current.children[scrollTo];
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, []);
+
+  return (
+    <ScrollArea.Root className="ScrollAreaRoot">
+      <ScrollArea.Viewport
+        className={isChild ? "ScrollAreaChild" : "ScrollAreaViewport"}
+      >
+        <div ref={scrollRef}>
+          {components.map((component, index) => (
+            <React.Fragment key={index}>{component}</React.Fragment>
+          ))}
+        </div>
+      </ScrollArea.Viewport>
+      <ScrollArea.Scrollbar
+        className="ScrollAreaScrollbar"
+        orientation="vertical"
+      >
+        <ScrollArea.Thumb className="ScrollAreaThumb" />
+      </ScrollArea.Scrollbar>
+      <ScrollArea.Scrollbar
+        className="ScrollAreaScrollbar"
+        orientation="horizontal"
+      >
+        <ScrollArea.Thumb className="ScrollAreaThumb" />
+      </ScrollArea.Scrollbar>
+      <ScrollArea.Corner className="ScrollAreaCorner" />
+    </ScrollArea.Root>
+  );
+};

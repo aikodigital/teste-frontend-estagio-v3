90a651d07dac2631ca41f013570d634e06aa5077
Leticia <leticiasilvaicm@gmail.com>
2023-03-29 15:43:20 -0300

    alterações na branch teste/leticia-leao

---
+++ js/Api.js
@@ -0,0 +1,75 @@
+const storeList =[
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-43.940933,  -19.912998]
+
+        },
+        
+
+        "properties": {
+          "name1": "Posição dos equipamentos",
+          "name": "CA-0001",
+          "date": "2021-02-01T03:00:00.000Z",
+          "lat": -19.126536,
+          "lon": -45.947756,
+
+          "id": "0808344c-454b-4c36-89e8-d7687e692d57",
+          "status": "<strong>Operando</strong>",
+          "color": "#2ecc71",
+          "equipmentStateId": "03b2d446-e3ba-4c82-8dc2-a5611fea6e1f",
+          "info":"Exibir no mapa os equipamentos nas suas posições mais recentes",
+          
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.6388,  -23.5489]
+
+        },
+        
+
+        "properties": {
+          "name1": "Estado atual do equipamento",
+          "name": "CA-0002",
+          "date": "2021-02-01T15:00:00.000Z",
+          "lat": -19.264235,
+          "lon": -46.092436,
+
+          "id": "0808344c-454b-4c36-89e8-d7687e692d57",
+          "status": "<strong>Manutenção</strong>",
+          "color":"#e74c3c",
+          "equipmentStateId": "03b2d446-e3ba-4c82-8dc2-a5611fea6e1f",
+          "info":"Visualizar o estado mais recente dos equipamentos. Exemplo: mostrando no mapa, como um pop-up, mouse hover sobre o equipamento, etc.",
+          
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ -43.2096,   -22.9035]
+
+        },
+        
+
+        "properties": {
+          "name1": "Histórico de estados do equipamento",
+          "name": "CA-0003",
+          "date": "2021-02-01T21:00:00.000Z",
+          "lat": -19.150549 ,
+          "lon": -45.999157,
+
+          "id": "0808344c-454b-4c36-89e8-d7687e692d57",
+          "status": "<strong>Parado</strong>",
+          "color": "#f1c40f",
+          "equipmentStateId": "03b2d446-e3ba-4c82-8dc2-a5611fea6e1f",
+          "info":"Permitir a visualização do histórico de estados de um equipamento específico ao clicar sobre o equipamento.",
+          
+        }
+      },
+          
+    ]
